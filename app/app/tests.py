5ac928019d46036d7e53f9e164a64e68f9f21dda
Chris Evans <chris.evans@astrion.us>
2025-08-20 16:53:39 -0500

    Fixed linting errors

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,19 +1,21 @@
 """
 Sample Test
 """
+
 from django.test import SimpleTestCase
+
 
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
+
     """Test che calc module."""
     def test_add_numbers(self):
         """testing adding numbers together"""
-        res = calc.add( 5, 6 )
-        self.assertEqual( res, 11 )
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
 
     def test_subtracting(self):
-        res = calc.subtract( 10, 15 )
-        
-        self.assertEqual(res, 5 )
-
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
