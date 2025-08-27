43921b94a78902ffbd47f50f0f932357f9f689e6
Chris Evans <chris.evans@astrion.us>
2025-08-27 10:55:48 -0500

    After lesson 40.  Tests failing as expected.

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,9 @@
+"""
+django command to wait for the db to be available
+"""
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """"""
+    def handle( self, *args, **options):
+        pass
