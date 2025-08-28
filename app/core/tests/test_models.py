a503d1bda353dba8e90c0b53a8c93cbbaacef084
Chris Evans <chris.evans@astrion.us>
2025-08-28 10:42:23 -0500

    Lesson 53 passed

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -35,3 +35,13 @@ class ModelTest(TestCase):
         """no email raises error """
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """test create a superuser """
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
